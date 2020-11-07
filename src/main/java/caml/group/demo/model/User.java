09137141785a8140e9cbf1f8f0a11b8f439985e2
Brandon2020 <45022235+Brandon2020@users.noreply.github.com>
2020-11-07 13:26:05 -0500

    Update User.java

--- src/main/java/caml/group/demo/model/User.java
+++ src/main/java/caml/group/demo/model/User.java
@@ -1,8 +1,15 @@
 package caml.group.demo.model;
 
 public class User {
+	String id;
+	String password; //Optional
 
-	public User() {
-		
+	public User(String id) {
+		this.id = id;
+	}
+	
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
 	}
 }
