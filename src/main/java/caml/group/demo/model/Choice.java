e36871344acb465c73c48469d1469a0eafa138d4
Brandon2020 <45022235+Brandon2020@users.noreply.github.com>
2020-11-07 13:25:31 -0500

    Update Choice.java

--- src/main/java/caml/group/demo/model/Choice.java
+++ src/main/java/caml/group/demo/model/Choice.java
@@ -1,8 +1,20 @@
 package caml.group.demo.model;
 
-public class Choice {
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 
-	public Choice() {
-		
+public class Choice {
+	final String id;
+	final String description;
+	final ArrayList<Alternative> alternatives;// = new ArrayList<Alternative>();
+	LocalDateTime time;
+	Alternative winner;
+
+	public Choice(String id, String description, ArrayList<Alternative> alternatives, LocalDateTime time) {
+		this.id = id;
+		this.description = description;
+		this.alternatives = alternatives;
+		this.time = time;
 	}
 }
+
