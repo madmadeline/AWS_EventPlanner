5c9046f15a3b9e6c42ae949807179a41f7702e98
jlrosenbaum <jlrosenbaum@wpi.edu>
2020-11-22 18:26:47 -0500

    Started work on handler

--- src/main/java/caml/group/demo/CreateChoiceHandler.java
+++ src/main/java/caml/group/demo/CreateChoiceHandler.java
@@ -1,8 +1,13 @@
 package caml.group.demo;
 
+import caml.group.demo.db.ChoiceDAO;
+import caml.group.demo.http.AddCreateChoiceRequest;
+import caml.group.demo.http.AddCreateChoiceResponse;
 import caml.group.demo.http.AddLogInRequest;
 import caml.group.demo.http.AddLogInResponse;
 
+import caml.group.demo.model.Alternative;
+import caml.group.demo.model.Choice;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -10,67 +15,35 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import caml.group.demo.db.UserDAO;
 //import caml.group.demo.model.Model;
 import caml.group.demo.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.internal.org.objectweb.asm.TypeReference;
 
-public class CreateChoiceHandler implements RequestHandler<AddLogInRequest,AddLogInResponse> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateChoiceHandler implements RequestHandler<AddCreateChoiceRequest,AddCreateChoiceResponse> {
 //	Model model;
 	LambdaLogger logger;
 	
 	
-	/**
-	 * Try to get User from RDS.
-	 */
-	public User loadUser(String name, String pass) throws Exception {
-		User user = null;
-		try {
-			if (logger != null) { logger.log("in loadUser"); }
-			UserDAO dao = new UserDAO(logger);
-			if (logger != null) { logger.log("retrieved DAO"); }
-			user = dao.getUser(name, pass);
-			if (logger != null) { logger.log("retrieved user"); }
-			return user;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return user;
+	public void createChoice(Choice choice) throws Exception {
+		logger.log("In createChoice in CreateChoiceHandler");
+		ChoiceDAO dao = new ChoiceDAO(logger);
+		logger.log("Retrieved Dao in CreateChoiceHandler");
 	}
 	
 	
 	@Override
-	public AddLogInResponse handleRequest(AddLogInRequest req, Context context) {
+	public AddCreateChoiceResponse handleRequest(AddCreateChoiceRequest req, Context context) {
 		logger = context.getLogger();
 		logger.log("Loading Java Lambda handler of RequestHandler");
 		logger.log(req.toString());
 
 		boolean fail = false;
 		String failMessage = "";
-		User user = null;
-		String name = "";
-		String pass = "";
-		
-		try {
-			name = req.getUsername();
-			try {
-				pass = req.getPassword();
-				user = loadUser(name, pass);
-			} catch (Exception e) {
-				failMessage = req.getPassword() + " is an invalid password.";
-				fail = true;
-			}
-		} catch (Exception e) {
-			failMessage = req.getUsername() + " is an invalid username.";
-			fail = true;
-		}
-
-		// compute proper response and return. Note that the status code is internal to the HTTP response
-		// and has to be processed specifically by the client code.
-		AddLogInResponse response;
-		if (fail) {
-			response = new AddLogInResponse(400, failMessage);
-		} else {
-			response = new AddLogInResponse(user, 200);  // success
-		}
-
-		return response; 
+		Choice choice = new Choice(req.getChoiceID(), req.getChoiceDescription(), null,
+				req.getDateOfCreation());
 	}
     
 
