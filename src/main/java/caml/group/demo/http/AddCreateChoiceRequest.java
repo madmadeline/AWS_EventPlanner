e881eabf7a5717baf687a1605d79a9543a816f00
jlrosenbaum <jlrosenbaum@wpi.edu>
2020-11-22 17:39:10 -0500

    Added functionality

--- src/main/java/caml/group/demo/http/AddCreateChoiceRequest.java
+++ src/main/java/caml/group/demo/http/AddCreateChoiceRequest.java
@@ -1,4 +1,30 @@
 package caml.group.demo.http;
 
+import java.sql.Timestamp;
+
 public class AddCreateChoiceRequest {
+    String choiceID;
+    String choiceDescription;
+    String altID;
+    String altDescription;
+    Timestamp dateOfCreation;
+    int numLikes = 0;
+    int numDislikes = 0;
+
+    public String getChoiceID() { return choiceID; }
+    public void setChoiceID(String id) { this.choiceID = id; }
+
+    public String getChoiceDescription() { return choiceDescription; }
+    public void setChoiceDescription(String description) { this.choiceDescription = description; }
+
+    public String getAltID() { return altID; }
+    public void setAltID(String id) { this.altID = id; }
+
+    public String getAltDescription() { return altDescription; }
+    public void setAltDescription(String description) { this.altDescription = description; }
+
+    public Timestamp getDateOfCreation() { return dateOfCreation; }
+    public void setDateOfCreation(Timestamp timestamp) { this.dateOfCreation = timestamp; }
+
+    public String toString() { return "Add(" + choiceID + ", " + dateOfCreation + ")"; }
 }
--- src/main/java/caml/group/demo/http/AddCreateChoiceResponse.java
+++ src/main/java/caml/group/demo/http/AddCreateChoiceResponse.java
@@ -1,4 +1,26 @@
 package caml.group.demo.http;
 
+import caml.group.demo.model.Choice;
+
 public class AddCreateChoiceResponse {
+    public boolean result;
+    public int statusCode;
+    public String error;
+
+    public AddCreateChoiceResponse(Choice choice, int statusCode){
+        this.result = choice != null;
+        this.statusCode = statusCode;
+        this.error = "";
+    }
+
+    public AddCreateChoiceResponse(int statusCode, String errorMessage){
+        this.result = false;
+        this.statusCode = statusCode;
+        this.error = errorMessage;
+    }
+
+    public String toString(){
+        if(statusCode == 200) return "Result(" + result + ")";
+        else return "ErrorResult(" + statusCode + ", err=" + error + ")";
+    }
 }
