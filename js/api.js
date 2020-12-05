7109fe7a50d12e0b975e74b96ce1f4a2cc98521d
madmadeline <mmperry@wpi.edu>
2020-12-05 13:24:45 -0500

    js

--- js/api.js
+++ js/api.js
@@ -4,9 +4,9 @@
 //MUST end in a SLASH
 var base_url = "https://24afvsw7sh.execute-api.us-east-1.amazonaws.com/alpha/";
 
-var find_choice_url = base_url + "choice"; //GET
+var find_choice_url = base_url + "find"; //POST
 var create_choice_url = base_url + "choice"; //POST
 var get_report_url = base_url + "report"; //GET
 var create_user_url = base_url + "user"; //post
-var list_users_url = base_url + "user"; //GET
 var send_approval_url = base_url + "approve"; //POST
+var delete_choice_url = base_url + "delete"; //POST
---
+++ js/deleteChoices.js
@@ -0,0 +1,52 @@
+function processDeleteResponse(result){
+	console.log("result:" + result);
+	var obj = JSON.parse(result);
+	var status = obj["statusCode"];
+	var deleteResult = document.getElementById("deleteResult");
+	if (status == 200){
+		console.log("Deletion successful!");
+		deleteResult.innerHTML = "Choices successfully deleted.";
+	}else{
+		console.log("Deletion unsuccessful.");
+		deleteResult.innerHTML = "Could not delete choices, or there were no choices to delete.";
+	}
+}
+
+function handleDeleteClick(){
+	//b is a boolean
+	//if true, an approval was chosen
+	//if false, a disapproval was chosen
+	
+	var data = {};
+	
+	days = document.form.deleteForm.deleteDays.value;
+	
+	data["days"] = days;
+	
+	var js = JSON.stringify(data);
+  	console.log("JS:" + js);
+  	var xhr = new XMLHttpRequest();
+  	xhr.open("POST", delete_choice_url, true);
+
+  	// send the collected data as JSON
+  	xhr.send(js);
+
+  	// This will process results and update HTML as appropriate. 
+  	xhr.onloadend = function () {
+	    console.log(xhr);
+	    console.log(xhr.request);
+	    if (xhr.readyState == XMLHttpRequest.DONE) {
+	    	 if (xhr.status == 200) {
+		      console.log ("XHR:" + xhr.responseText);
+		      processDeleteResponse(xhr.responseText);
+	    	 } else {
+	    		 console.log("actual:" + xhr.responseText)
+				  var js = JSON.parse(xhr.responseText);
+				  var err = js["response"];
+				  alert (err);
+	    	 }
+	    } else {
+	      processDeleteResponse("N/A");
+	    }
+	  };
+}
