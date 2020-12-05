453da54cf23195837a7e2460da77605c6efe865f
bsnap <bsnap@130.215.250.146>
2020-12-04 21:24:53 -0500

    u8jol

--- src/main/java/caml/group/demo/db/FeedbackDAO.java
+++ src/main/java/caml/group/demo/db/FeedbackDAO.java
@@ -164,7 +164,7 @@ public class FeedbackDAO {
 
             // add to the Feedback table
             ps = conn.prepareStatement("INSERT INTO " + feedbackTbl +
-                    " (altID,userID,message,timeStamp,approved) values(?,?,?,?,?);");
+                    " (altID,userID,message,timeStamp,approved) values(?,?,?,?,?);"); // ps is closed ignore error
             ps.setString(1, altID);
             ps.setString(2, userID);
             ps.setString(3, message);
